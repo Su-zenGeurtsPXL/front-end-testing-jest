1a558ed241c7d5923d6b87ab025b70634526c6b1
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-06-13 16:50:56 +0200

    Deleted unrelevant code

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Page from '@/views/Page'
 import HomeView from '@/views/HomeView'
 
 Vue.use(Router)
@@ -8,12 +7,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/page',
-      name: 'Page',
-      component: Page
-    },
-    {
-      path: '/home',
+      path: '/',
       name: 'HomeView',
       component: HomeView
     }
